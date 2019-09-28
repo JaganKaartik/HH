97e38a95fc469698e49a32e383f01d2da5bb7e94
JaganKaartik <kaartikjagan@gmail.com>
2019-09-29 01:39:30 +0530

    Updated JavaPackage

--- src/java/HealthRecord/user.java
+++ src/java/HealthRecord/user.java
@@ -3,6 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package HealthRecord;
+
+/**
+ *
+ * @author jagankaartik58
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /**
  *
