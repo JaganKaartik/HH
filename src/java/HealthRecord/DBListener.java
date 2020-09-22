c10c66b56dddb31f8c60fdebf7d2b1de7e545a24
JugHead! <kaartikjagan@gmail.com>
2020-09-23 02:26:25 +0530

    Update DBListener.java

--- src/java/HealthRecord/DBListener.java
+++ src/java/HealthRecord/DBListener.java
@@ -18,19 +18,11 @@ public class DBListener implements ServletContextListener
     public void contextInitialized(ServletContextEvent event) 
     {            
 		try{  
-				
-<<<<<<< HEAD
 			Class.forName("");
 			String db_url = "";
-	        String db_username = ""; 
+	       		String db_username = ""; 
 			String db_password = "";
-=======
-			Class.forName("org.postgresql.Driver");
-			String db_url = "jdbc:postgresql://localhost:5432/Electronic_Health_Record";
-	        String db_username = "postgres";
-			String db_password = "qpalzmwer";
->>>>>>> 10faebbe3093be6076162497769d6ec7af271e02
-            Connection con = DriverManager.getConnection(db_url,db_username,db_password);  
+            		Connection con = DriverManager.getConnection(db_url,db_username,db_password);  
 
 			//storing connection object as an attribute in ServletContext  
 				
